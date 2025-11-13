f361a32e9d95b13d3ffbb3b58fe785d85045af04
indunath mishra <154290109+Imishra8562@users.noreply.github.com>
2025-11-13 09:27:10 +0530

    design completed

---
+++ Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Web;
+using System.Web.Mvc;
+using Web.Controllers;
+
+namespace Web.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        #region Get System IP
+        public string SystemIP()
+        {
+            string Ipaddress = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(Ipaddress))
+            {
+                Ipaddress = Request.ServerVariables["REMOTE_ADDR"];
+            }
+            return Ipaddress;
+        }
+
+        #endregion
+        #region Header
+        public ActionResult Header()
+        {
+            return PartialView("_Header");
+        }
+        #endregion
+
+        #region Menu
+        public ActionResult Menu()
+        {
+            return PartialView("_Menu");
+        }
+        #endregion
+
+        #region Footer
+        public ActionResult Footer()
+        {
+            return PartialView("_Footer");
+        }
+        #endregion
+        #region Footer
+        public ActionResult EmployeeDashboard()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
--- Web/Controllers/Home/HomeController.cs
+++ Web/Controllers/Home/HomeController.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        #region Layout & Home
         public ActionResult Index()
         {
             return View();
@@ -21,19 +24,102 @@ namespace Web.Controllers
         {
             return PartialView("_Footer");
         }
+
+        #endregion
+
+        #region about
         public ActionResult About()
         {
             return View();
         }
-        public ActionResult Contact()
+        public ActionResult OurTeam()
         {
             return View();
         }
-        public ActionResult Service()
+        public ActionResult Overview()
         {
             return View();
         }
-        public ActionResult Blog()
+        #endregion
+
+        #region Industries
+        public ActionResult Industries()
+        {
+            return View();
+        }
+        #endregion
+
+        #region news & insight
+        public ActionResult Newsroom()
+        {
+            return View();
+        }
+        public ActionResult PressReleases()
+        {
+            return View();
+        }
+        public ActionResult Insights()
+        {
+            return View();
+        }
+        public ActionResult PulseJournal()
+        {
+            return View();
+        }
+        #endregion
+
+        #region Training,Events & Careers
+        public ActionResult Training()
+        {
+            return View();
+        }
+        public ActionResult Events()
+        {
+            return View();
+        }
+        public ActionResult Courses()
+        {
+            return View();
+        }
+        public ActionResult PastWebinars()
+        {
+            return View();
+        }
+       
+        public ActionResult Careers()
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region KnowledgeBase
+        public ActionResult KnowledgeBase()
+        {
+            return View();
+        }
+        #endregion
+
+        #region EngineeringServices
+        public ActionResult EngineeringServices()
+        {
+            return View();
+        }
+        #endregion
+
+        #region InvestorRelations & Sustainability
+        public ActionResult InvestorRelations()
+        {
+            return View();
+        }
+        public ActionResult Sustainability()
+        {
+            return View();
+        }
+        #endregion
+
+        #region ContactUs & TermAndCondition
+        public ActionResult ContactUs()
         {
             return View();
         }
@@ -41,9 +127,14 @@ namespace Web.Controllers
         {
             return View();
         }
+        #endregion
+
+        #region login & register
         public ActionResult Login()
         {
-            return View(); 
+            return View();
         }
+        #endregion
+
     }
 }
