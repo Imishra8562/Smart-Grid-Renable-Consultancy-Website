a5a59408b31f96111bb695b898e94f5792e13dd2
Imishra8562 <indunathmishra54@gmail.com>
2026-01-13 18:53:43 +0530

    1/13/26 All error page done

---
+++ Web/Controllers/Error/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    [AllowAnonymous]
+    public class ErrorController : Controller
+    {
+        public ActionResult General()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+        public ActionResult AccessDenied()
+        {
+            Response.StatusCode = 403;
+            return View();
+        }
+
+        public ActionResult ServerError()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
